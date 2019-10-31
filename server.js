8e6a3c6c470e8fbb214243e8c697e031caae3030
Tonggyu Kim <x-zezima@hotmail.com>
2019-10-30 21:46:31 -0700

    changed port

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ app.set('view engine', 'handlebars');
 var routes = require('./controllers/routes.js');
 app.use('/', routes);
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 app.listen(port);
