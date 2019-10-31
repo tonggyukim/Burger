af8a2dacd5a39ef3f3d60cef43d183d31da04e2c
Tonggyu Kim <x-zezima@hotmail.com>
2019-10-30 20:59:16 -0700

    changed connection details

--- config/connection.js
+++ config/connection.js
@@ -10,10 +10,10 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'tony',
-        database: 'burgers_db'
+        host: 'l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: '	y7akqu6ig2kg8jy5',
+        password: 'e6nw4nbbbifw4ip7',
+        database: 'bi3buz330bfq1mmq'
     })
 }
 connection.connect(function(err){
