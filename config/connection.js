32ef96b9b3f431582aefe7ad083bedcb9b09eaf0
Tonggyu Kim <x-zezima@hotmail.com>
2019-10-30 20:40:56 -0700

    hooked project with JawsDB

--- config/connection.js
+++ config/connection.js
@@ -6,6 +6,16 @@ var connection = mysql.createConnection({
      database:'burgers_db'
 });
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'tony',
+        database: 'burgers_db'
+    })
+}
 connection.connect(function(err){
     if(err)throw err;
     console.log('connected as id: '+connection.threadId);
